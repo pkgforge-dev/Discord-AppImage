0b74e62bde1f4a5481ca1611bbb481011050a112
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-15 21:33:29 -0400

    bundle `jq` for hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,6 +16,7 @@ export DEPLOY_VULKAN=1
 # Deploy dependencies
 quick-sharun \
 	./AppDir/bin/* \
+	/usr/bin/jq    \
 	/usr/lib/libappindicator3.so*
 
 # Additional changes can be done in between here
