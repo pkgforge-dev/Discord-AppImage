d97468e5e88ce757f07d333fc3cd8c71b0980797
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-12 06:32:46 -0400

    add test and make sure the app can run in the CI

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,6 +12,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_PULSE=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+export ELECTRON_DISABLE_SANDBOX=1
 
 # Deploy dependencies
 quick-sharun \
@@ -23,3 +24,7 @@ quick-sharun \
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --test ./dist/*.AppImage
