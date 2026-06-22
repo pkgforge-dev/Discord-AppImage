77b5df9497922823f9a0b1e18aebea1f3dff525f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-22 13:33:58 -0400

    fix?

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,9 +18,14 @@ export DEPLOY_VULKAN=1
 sed -i -e 's|/usr/bin/discord|Discord|g' /usr/share/applications/discord.desktop
 
 # discord arch package now downloads the electron bundle in the user's home
-/usr/bin/discord
+# the bootstrap script will downlaod discord and attempt to execute it which will fail
+/usr/bin/discord || :
 
 discord_exe=$(find "${XDG_CONFIG_HOME:-$HOME/.config}"/discord -type f -name Discord -print -quit)
+if [ ! -x "$discord_exe" ]; then
+	>&2 echo "Cannot find discord, download failed?"
+	exit 1
+fi
 discord_dir=${discord_exe%/*}
 
 # Deploy dependencies
